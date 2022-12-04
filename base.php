38d1e54478f66bdffbc0559c4976a8b9d80b2025
Hafizh Wardoyo <117241344+Fizhh06@users.noreply.github.com>
2022-12-04 22:45:10 +0700

    Add files via upload

---
+++ base.php
@@ -0,0 +1,41 @@
+<?php
+/**
+* Base rule file Uinsu-Web-Framework
+*/
+
+/**
+* URL base, jika di upload ke hosting, perhatikan http/https, karena akan berpengaruh
+*/
+$homeBase = 'http://localhost/Ganteng-Resto/';
+
+/**
+* Site name (nama situs web)
+*/
+$siteName = 'Ganteng Restoran';
+
+/**
+* Author (Pengembang / pemilik website)
+*/
+$author = 'Si Ganteng';
+
+/**
+* Default route
+*/
+$mainRoute = 'login';
+
+/**
+* Path to  style asset (css, js, dll)
+*/
+$styleBase = $homeBase.'ladun';
+
+/**
+* Define to public
+*/
+define('HOMEBASE', $homeBase);
+define('SITENAME', $siteName);
+define('AUTHOR', $author);
+define('MAINROUTE', $mainRoute);
+define('STYLEBASE', $styleBase);
+
+
+
