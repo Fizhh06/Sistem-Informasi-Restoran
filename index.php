a79b6b8c27738be33d3551783afcc99e1e5c5ad4
Hafizh Wardoyo <117241344+Fizhh06@users.noreply.github.com>
2022-12-04 22:12:21 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+
+* @package	
+* @author	
+* @link		
+* @since	
+*/
+
+function __construct(){
+	$getSelfData = [];
+}
+
+/**
+* Call all function with init.php file
+*/
+require_once 'engine/init.php';
+
+$props = new Props;
